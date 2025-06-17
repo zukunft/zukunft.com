085411d4b4ba4898b8fc4b426b80bfba46276c61
timon <timon@zukunft.com>
2025-06-17 20:26:20 +0200

    use sudo instead of runuser in debian (part of fix #133)

--- src/main/php/cfg/db/db_check.php
+++ src/main/php/cfg/db/db_check.php
@@ -84,6 +84,7 @@ class db_check
      */
     function db_check(sql_db $db_con): user_message
     {
+        global $cfg;
 
         $usr_msg = new user_message(); // the message that should be shown to the user immediately
         $do_consistency_check = false;
@@ -93,11 +94,11 @@ class db_check
         // TODO remove rewrite before moved to PROD
         $main_tbl_name = $lib->class_to_name(config::class);
         if (!$db_con->has_table($main_tbl_name)) {
+            $cfg = new config();
             $usr_msg = $db_con->setup_db();
             if ($usr_msg->is_ok()) {
                 $db_con->db_fill_code_links();
                 $db_con->db_check_missing_owner();
-                $cfg = new config();
                 $cfg->set(config::LAST_CONSISTENCY_CHECK, gmdate(DATE_ATOM), $db_con);
             }
         }
