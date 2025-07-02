07d2d5395ad2b39b275e2aa2a9edaa72d88d03dd
Ivc <vank300828@gmail.com>
2025-07-02 14:03:53 +0200

    upd install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,17 @@
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 NC="\033[0m"
-
+PGSQL_USERNAME=zukunft_db_root
+PGSQL_PASSWORD=mysecretpassword
+PGSQL_DATABASE=zukunft
+WWW_ROOT=/var/www/html
+LOCAL_USER=${SUDO_USER:-$USER}
 
 # ------------------------------------------
 ##  START Main
 # ------------------------------------------
 main() {
-    rootCheck
+    #rootCheck
 
     # Set current directory
     CURRENT_DIR=$(pwd)
@@ -34,8 +38,9 @@ main() {
         installAndConfigurePostgresql
         installAndConfigureApache
         installAndConfigurePhp
-        downloadAndInstallExternalLibraries
         downloadAndInstallZukunft
+        downloadAndInstallExternalLibraries
+        runZukunftApp
         #testInstallation
     else
         if [[ "$OS" == "docker" ]]; then
@@ -65,7 +70,7 @@ rootCheck() {
 }
 
 displayIntro() {
-    clear ">$(tty)"
+    clear > "$(tty)"
 
     # Initial prompt
     echo -e "${GREEN}ZUKUNFT INSTALLER${NC}"
@@ -166,10 +171,10 @@ updateDebian() {
     echo -e "\n${GREEN}Updating debian...${NC}"
 
     # Update Debian
-    apt-get update && apt-get upgrade
+    sudo apt-get update -y && sudo apt-get upgrade -y
 
     # make sure that git is installed
-    apt-get install -y git
+    sudo apt-get install -y git
 }
 
 installAndConfigurePostgresql() {
@@ -177,15 +182,25 @@ installAndConfigurePostgresql() {
 
     # Install postgres
     # TODO check if postgres is already installed and if yes request the user and password once to create a zukunft user and a db
-    apt-get install -y postgresql postgresql-contrib
+    sudo apt-get update && sudo apt-get install -y postgresql postgresql-contrib
+    sudo systemctl start postgresql
+    sudo systemctl enable postgresql
+
+    # Create user if not exists
+    sudo -u postgres psql -c "DO \$\$
+    BEGIN
+       IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$PGSQL_USERNAME') THEN
+           CREATE ROLE $PGSQL_USERNAME LOGIN PASSWORD '$PGSQL_PASSWORD';
+       END IF;
+    END
+    \$\$;"
 
     # Initialize database
     # TODO if no password is given just create on and write it to the .env secrets
     # TODO use the generated or give db password in the php code
     # TODO add postgres admin username and password if postgres is ready running and the standard user name is changed
     # TODO secure the standard postgres user name after install
-    # create the user
-    sudo -u postgres psql -d postgres -U postgres -c "CREATE USER $PGSQL_USERNAME WITH PASSWORD '$PGSQL_PASSWORD';"
+    
     # drop any old database with the same name
     sudo -u postgres psql -d postgres -U postgres -c "DROP DATABASE $PGSQL_DATABASE"
     # create the database
@@ -202,28 +217,28 @@ installAndConfigureApache() {
     echo -e "\n${GREEN}Installing Apache...${NC}"
 
     # Install Apache
-    apt-get install -y apache2
+    sudo apt-get install -y apache2
 
-    systemctl enable apache2
-    systemctl start apache2
+    sudo systemctl enable apache2
+    sudo systemctl start apache2
 }
 
 installAndConfigurePhp() {
     echo -e "\n${GREEN}Installing PHP ...${NC}"
 
     # Install PHP
-    apt-get install -y php
-    apt-get install -y php-pgsql
-    apt-get install -y php-yaml
-    apt-get install -y php-curl
-    apt-get install -y php-xml
-    apt-get install -y php-json
+    sudo apt-get install -y php
+    sudo apt-get install -y php-pgsql
+    sudo apt-get install -y php-yaml
+    sudo apt-get install -y php-curl
+    sudo apt-get install -y php-xml
+    sudo apt-get install -y php-json
 
     PHP_VERSION=$(php -r 'echo PHP_VERSION;' | cut -d. -f1,2)
-    if [[ "$PHP_VERSION" != "8.2" ]]; then
-        echo -e "${RED}PHP 8.2 is required, found $PHP_VERSION${NC}"
-        exit 1
-    fi
+    #if [[ "$PHP_VERSION" != "8.2" ]]; then
+    #    echo -e "${RED}PHP 8.2 is required, found $PHP_VERSION${NC}"
+    #    exit 1
+    #fi
     echo -e "Installed PHP: \n$(php --version)"
     sleep 3
 }
@@ -231,24 +246,51 @@ installAndConfigurePhp() {
 downloadAndInstallExternalLibraries() {
     echo -e "\n${GREEN}Installing external libraries ...${NC}"
 
+    cd $WWW_ROOT
+
     echo -e "\n${GREEN}Installing bootstrap ...${NC}"
-    git clone https://github.com/twbs/bootstrap.git
+    git clone --branch v4.1.3 --depth 1 https://github.com/twbs/bootstrap.git
+    mkdir -p "$WWW_ROOT/lib_external/bootstrap/4.1.3/"
     rsync -av --delete bootstrap/ "$WWW_ROOT/lib_external/bootstrap/4.1.3/"
 
     echo -e "\n${GREEN}Installing fontawesome ...${NC}"
     git clone https://github.com/gabrielelana/awesome-terminal-fonts
+    mkdir -p "$WWW_ROOT/lib_external/fontawesome/"
     rsync -av --delete awesome-terminal-fonts/ "$WWW_ROOT/lib_external/fontawesome/"
     sleep 3
 }
 
 downloadAndInstallZukunft() {
     echo -e "\n${GREEN}Installing zukunft.com ...${NC}"
+    sudo mkdir -p $WWW_ROOT
+    
+    sudo chown -R "$LOCAL_USER":"$LOCAL_USER" "$WWW_ROOT/"
+
+    # switch later to something like git://git.zukunft.com/zukunft.git
+    git clone -b "$BRANCH" https://github.com/zukunft/zukunft.com "$WWW_ROOT/"
+}
+
+installZukunftInDocker() {
+    echo -e "\n${GREEN}Installing zukunft.com in docker ...${NC}"
 
     # switch later to something like git://git.zukunft.com/zukunft.git
     git clone -b "$BRANCH" https://github.com/zukunft/zukunft.com "$WWW_ROOT/"
 
+    cd "$WWW_ROOT" || exit
+
+    echo -e "\n${GREEN}Building docker images ...${NC}"
+
+    docker compose up -d --build
+
+    echo -e "\n${GREEN}Resetting database ...${NC}"
+
+    docker compose exec app php test/reset_db.php
+
+}
+
+runZukunftApp() {
     # force to reread to www root ?
-    systemctl restart apache2
+    sudo systemctl restart apache2
 
     # create the zukunft.com database tables
     php "$WWW_ROOT/test/reset_db.php"
@@ -269,24 +311,6 @@ downloadAndInstallZukunft() {
 
     cd "$CURRENT_DIR" || exit
     sleep 3
-}
-
-installZukunftInDocker() {
-    echo -e "\n${GREEN}Installing zukunft.com in docker ...${NC}"
-
-    # switch later to something like git://git.zukunft.com/zukunft.git
-    git clone -b "$BRANCH" https://github.com/zukunft/zukunft.com "$WWW_ROOT/"
-
-    cd "$WWW_ROOT" || exit
-
-    echo -e "\n${GREEN}Building docker images ...${NC}"
-
-    docker compose up -d --build
-
-    echo -e "\n${GREEN}Resetting database ...${NC}"
-
-    docker compose exec app php test/reset_db.php
-
 }
 
 # ------------------------------------------
