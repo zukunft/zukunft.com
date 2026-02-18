01c5f7a07ed648c2a76606c9f88538c2e16a6e32
timon <timon@zukunft.com>
2026-02-18 15:05:42 +0100

    deprecate insert_old as part of fix #232

--- src/test/resources/db/type/job_status_insert_log_11111.sql
+++ src/test/resources/db/type/job_status_insert_log_11111.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION job_status_insert_log_1111
+CREATE OR REPLACE FUNCTION job_status_insert_log_11111
     (_status_name             text,
      _user_id                 bigint,
      _change_action_id        smallint,
@@ -6,7 +6,9 @@ CREATE OR REPLACE FUNCTION job_status_insert_log_1111
      _field_id_code_id        smallint,
      _code_id                 text,
      _field_id_description    smallint,
-     _description             text) RETURNS bigint AS
+     _description             text,
+     _field_id_priority       smallint,
+     _priority                smallint) RETURNS bigint AS
 $$
 DECLARE new_job_status_id bigint;
 BEGIN
@@ -24,9 +26,13 @@ BEGIN
         INSERT INTO changes ( user_id, change_action_id, change_field_id,      new_value,   row_id)
              SELECT          _user_id,_change_action_id,_field_id_description,_description, new_job_status_id ;
 
+        INSERT INTO changes ( user_id, change_action_id, change_field_id,      new_value,   row_id)
+             SELECT          _user_id,_change_action_id,_field_id_priority,   _priority,    new_job_status_id ;
+
              UPDATE job_statuus
                 SET code_id     = _code_id,
-                    description = _description
+                    description = _description,
+                    priority    = _priority
               WHERE job_statuus.job_status_id = new_job_status_id;
 
              RETURN new_job_status_id;
@@ -34,12 +40,12 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-PREPARE job_status_insert_log_1111_call
-    (text,bigint,smallint,smallint,smallint,text,smallint,text) AS
-SELECT job_status_insert_log_1111
-    ($1,$2,$3,$4,$5,$6,$7,$8);
+PREPARE job_status_insert_log_11111_call
+    (text,bigint,smallint,smallint,smallint,text,smallint,text,smallint,smallint) AS
+SELECT job_status_insert_log_11111
+    ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);
 
-SELECT job_status_insert_log_1111
+SELECT job_status_insert_log_11111
     ('created'::text,
      1::bigint,
      1::smallint,
@@ -47,4 +53,6 @@ SELECT job_status_insert_log_1111
      856::smallint,
      'new'::text,
      857::smallint,
-     'the job is not yet assigned to any calc engine'::text);
+     'the job is not yet assigned to any calc engine'::text,
+     858::smallint,
+     0::smallint);
